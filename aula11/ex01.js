de4e06ef4adaa9090bce8c697636329f2b759a14
Bruna Miranda <166440150+fbrunamiranda@users.noreply.github.com>
2024-07-04 10:06:17 -0300

    Initial commit

---
+++ aula11/ex01.js
@@ -0,0 +1,8 @@
+var vel = 78.2
+
+console.log(`A velocidade do seu carrp é ${vel} KM/H`)
+
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex02.js
@@ -0,0 +1,8 @@
+var pais = `EUA`
+console.log(`Viviendo em ${pais}`)
+
+if(pais != `Brasil`){
+    console.log(`Você é Estrangeiro!`)
+}else{
+    console.log(`Você é brasileiro`)
+}
